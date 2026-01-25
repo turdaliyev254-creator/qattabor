28ed0e44c1f756daab06c0d33195dafcaa055203
turdaliyev254-creator <turdaliyev254@gmail.com>
2026-01-26 02:08:17 +0500

    icon fixed

--- app/Http/Controllers/Admin/SubcategoryController.php
+++ app/Http/Controllers/Admin/SubcategoryController.php
@@ -155,7 +155,7 @@ class SubcategoryController extends Controller
 
         $validated['slug'] = Str::slug($validated['name']);
         $validated['category_id'] = $category->id;
-        $validated['order'] = Subcategory::where('category_id', $category->id)->max('order') + 1;
+        $validated['order'] = Subcategory::where('category_id', $category->id)->max('order') + 1 ?? 0;
 
         Subcategory::create($validated);
 
