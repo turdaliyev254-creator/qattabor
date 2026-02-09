51766566645e7d1c2ce5b620f3490298fc37e226
turdaliyev254-creator <turdaliyev254@gmail.com>
2026-02-09 11:49:58 +0500

    fixed image

--- resources/views/places/show.blade.php
+++ resources/views/places/show.blade.php
@@ -2,14 +2,14 @@
     <!-- Hero Section with Media Gallery (2GIS Style) -->
     <div class="relative -mx-4 -mt-6 mb-8" x-data="{ 
         currentMedia: 0,
-        media: {{ Js::from($place->images->map(function($img) {
+        media: @json($place->images->map(function($img) {
             return [
                 'type' => $img->media_type,
                 'url' => $img->media_type === 'video' ? asset('storage/' . $img->image_path) : asset('storage/' . $img->image_path),
                 'thumbnail' => $img->thumbnail_url ? asset('storage/' . $img->thumbnail_path) : asset('storage/' . $img->image_path),
                 'duration' => $img->formatted_duration
             ];
-        })->toArray()) }},
+        })->toArray()),
         showFullscreen: false,
         showThumbnails: false,
         isPlaying: false,
